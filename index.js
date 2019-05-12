4a1f67a322b0344a08324892468d5b77fd8a9d8e
aw <andy@nonissue.org>
2019-05-11 23:30:50 -0600

    added functionality to search for stop in tripupdates

--- index.js
+++ index.js
@@ -2,34 +2,43 @@ var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
 
 // If you want to view the data from a different request
-// Just uncomment the one you want to view and make sure 
+// Just uncomment the one you want to view and make sure
 // the other two are commented
 var requestSettings = {
   method: 'GET',
   // trip updates:
-  // url: 'https://data.edmonton.ca/download/uzpc-8bnm/application%2Foctet-stream',
+  url: 'https://data.edmonton.ca/download/uzpc-8bnm/application%2Foctet-stream',
 
   // vehicle positions:
-  url: 'https://data.edmonton.ca/download/7qed-k2fc/application%2Foctet-stream',
-  
+  //url: 'https://data.edmonton.ca/download/7qed-k2fc/application%2Foctet-stream',
+
   // trip alerts:
   // url: 'https://data.edmonton.ca/download/rqaa-jae6/application%2Foctet-stream',
-  encoding: null
+  encoding: null,
 };
-request(requestSettings, function (error, response, body) {
+request(requestSettings, function(error, response, body) {
   if (!error && response.statusCode == 200) {
     var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+    var filtered = [];
     feed.entity.forEach(function(entity) {
       if (entity) {
-        console.log(entity.toJSON());
+        var tripJson = entity.toJSON();
+        var tripStops = tripJson.tripUpdate.stopTimeUpdate;
+        tripStops.filter(stop => {
+          // search for a specific stop
+          // I picked the one below at random
+          if (stop.stopId === '3121') {
+            filtered.push(stop);
+          }
+        });
       }
     });
+    console.log(filtered);
   } else if (error) {
-    console.log("error!");
+    console.log('error!');
   } else {
-    // We shouldn't hit this?
-    console.log("Request failed?");
+    console.log('Request failed?');
   }
 });
 
-console.log("Fetching data...")
+// console.log("Fetching data...")
