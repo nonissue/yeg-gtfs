933180b513eb9991f322847a13c83b54fd049ffc
aw <andy@nonissue.org>
2019-05-12 00:03:09 -0600

    fixed alerts fetching

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
 
 const dataEndpoints = [
-  { name: 'tripUpdates', url: 'https://data.edmonton.ca/download/uzpc-8bnm/application%2Foctet-stream'},
-  { name: 'vehiclePos', url: 'https://data.edmonton.ca/download/7qed-k2fc/application%2Foctet-stream'},
-  { name: 'alerts', url: 'https://data.edmonton.ca/download/rqaa-jae6/application%2Foctet-stream'},
-]
+  {
+    name: 'tripUpdates',
+    url:
+      'https://data.edmonton.ca/download/uzpc-8bnm/application%2Foctet-stream',
+  },
+  {
+    name: 'vehiclePos',
+    url:
+      'https://data.edmonton.ca/download/7qed-k2fc/application%2Foctet-stream',
+  },
+  {
+    name: 'alerts',
+    url:
+      'https://data.edmonton.ca/download/rqaa-jae6/application%2Foctet-stream',
+  },
+];
 
 function getData(url) {
   const requestSettings = {
@@ -13,31 +25,33 @@ function getData(url) {
     url: url,
     encoding: null,
   };
-  
+
   request(requestSettings, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-      var filtered = [];
-      feed.entity.forEach(function(entity) {
-        // console.log(entity)
-        if (entity) {
-          var resJSON = entity.toJSON();
-          if (resJSON.tripUpdate) {
-            var tripStops = resJSON.tripUpdate.stopTimeUpdate;
-            tripStops.filter(stop => {
-              // search for a specific stop
-              // I picked the one below at random
-              if (stop.stopId) {
-                filtered.push(stop);
-              }
-            });
-          } else {
-            console.log(resJSON);
+      if (url === dataEndpoints[2].url) {
+        console.log('Alerts: \n');
+        console.log(feed);
+      } else {
+        var filtered = [];
+        feed.entity.forEach(function(entity) {
+          if (entity) {
+            var resJSON = entity.toJSON();
+            if (resJSON.tripUpdate) {
+              var tripStops = resJSON.tripUpdate.stopTimeUpdate;
+              tripStops.filter(stop => {
+                if (stop.stopId) {
+                  filtered.push(stop);
+                }
+              });
+            } else {
+              console.log(resJSON);
+            }
           }
+        });
+        if (filtered.length != 0) {
+          console.log(filtered);
         }
-      });
-      if (filtered.length != 0) {
-        console.log(filtered);
       }
     } else if (error) {
       console.log('error!');
