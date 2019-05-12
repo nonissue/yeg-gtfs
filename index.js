a437af4ec8d5df965b28e07aa8bc145b157094f5
aw <andy@nonissue.org>
2019-05-11 23:53:44 -0600

    added sone basic interactiveness at command line

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
 
-// If you want to view the data from a different request
-// Just uncomment the one you want to view and make sure
-// the other two are commented
-var requestSettings = {
-  method: 'GET',
-  // trip updates:
-  url: 'https://data.edmonton.ca/download/uzpc-8bnm/application%2Foctet-stream',
+const dataEndpoints = [
+  { name: 'tripUpdates', url: 'https://data.edmonton.ca/download/uzpc-8bnm/application%2Foctet-stream'},
+  { name: 'vehiclePos', url: 'https://data.edmonton.ca/download/7qed-k2fc/application%2Foctet-stream'},
+  { name: 'alerts', url: 'https://data.edmonton.ca/download/rqaa-jae6/application%2Foctet-stream'},
+]
 
-  // vehicle positions:
-  //url: 'https://data.edmonton.ca/download/7qed-k2fc/application%2Foctet-stream',
-
-  // trip alerts:
-  // url: 'https://data.edmonton.ca/download/rqaa-jae6/application%2Foctet-stream',
-  encoding: null,
-};
-request(requestSettings, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-    var filtered = [];
-    feed.entity.forEach(function(entity) {
-      if (entity) {
-        var tripJson = entity.toJSON();
-        var tripStops = tripJson.tripUpdate.stopTimeUpdate;
-        tripStops.filter(stop => {
-          // search for a specific stop
-          // I picked the one below at random
-          if (stop.stopId === '3121') {
-            filtered.push(stop);
+function getData(url) {
+  const requestSettings = {
+    method: 'GET',
+    url: url,
+    encoding: null,
+  };
+  
+  request(requestSettings, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+      var filtered = [];
+      feed.entity.forEach(function(entity) {
+        // console.log(entity)
+        if (entity) {
+          var resJSON = entity.toJSON();
+          if (resJSON.tripUpdate) {
+            var tripStops = resJSON.tripUpdate.stopTimeUpdate;
+            tripStops.filter(stop => {
+              // search for a specific stop
+              // I picked the one below at random
+              if (stop.stopId) {
+                filtered.push(stop);
+              }
+            });
+          } else {
+            console.log(resJSON);
           }
-        });
+        }
+      });
+      if (filtered.length != 0) {
+        console.log(filtered);
       }
-    });
-    console.log(filtered);
-  } else if (error) {
-    console.log('error!');
-  } else {
-    console.log('Request failed?');
-  }
+    } else if (error) {
+      console.log('error!');
+    } else {
+      console.log('Request failed?');
+    }
+  });
+}
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-// console.log("Fetching data...")
+readline.question(
+  `\n┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n┃ Which data do you want?\t\t┃\n┃ Enter 1, 2 or 3 \t\t\t┃\n┃ 1: tripUpdates, 2: vehicle, 3: alerts ┃ \n┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n\n-> `,
+  dataType => {
+    console.log(`Fetching ${dataEndpoints[parseInt(dataType) - 1].name}!`);
+    getData(dataEndpoints[parseInt(dataType) - 1].url);
+    readline.close();
+  }
+);
